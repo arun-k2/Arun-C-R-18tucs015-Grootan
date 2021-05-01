8cee3e30f732237d65a2f100589ac969696b129f
arun-k2 <arundgj2000@gmail.com>
2021-05-01 10:54:00 +0530

    changed title

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => (
   <div className='container mt-4'>
     <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
+       Grootan File Upload
     </h4>
 
     <FileUpload />
